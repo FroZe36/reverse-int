2336f299a55c4edc164052bd90f27a8754dfbe72
Danila <AngelofDest1ny@yandex.ru>
2023-08-03 22:23:47 +0300

    complete test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+  let reverse = n.toString();
+  return reverse.includes('-') ? reverse.slice(1).split('').reverse().join('') : reverse.split('').reverse().join('')
 }
